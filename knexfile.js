a5040848a882c12d04f6666069bdce70e7f958a7
Patrick Biffle <pbiffle@gmail.com>
2018-01-22 09:51:15 -0700

    changed live server to production environmnet in knex file

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,12 @@
 module.exports = {
 	development: {
 		client: 'pg',
-		connection:
-			'postgres://cbvpgmjzvtokgl:962c46ab807cb656e6b32812e7a638d9745bcc68bffdabd5d0ce1b7cfea50685@ec2-107-20-204-179.compute-1.amazonaws.com:5432/de0s4m738p3mbm'
+		connection: 'postgres:///3000'
 	},
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL
+		connection:
+			process.env.DATABASE_URL ||
+			'postgres://cbvpgmjzvtokgl:962c46ab807cb656e6b32812e7a638d9745bcc68bffdabd5d0ce1b7cfea50685@ec2-107-20-204-179.compute-1.amazonaws.com:5432/de0s4m738p3mbm'
 	}
 }
